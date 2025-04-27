3ed5152117ac7fe7df38b45e42da8d573098e592
tjdanqkr <tjdanqkr@gmail.com>
2025-04-27 16:16:38 +0900

    feature: validation 추가

--- src/main/java/com/plus/profile/user/presentation/UserController.java
+++ src/main/java/com/plus/profile/user/presentation/UserController.java
@@ -1,12 +1,12 @@
 package com.plus.profile.user.presentation;
 
 import com.plus.profile.global.dto.ApiResponse;
+import com.plus.profile.global.dto.CreatePaymentResponse;
 import com.plus.profile.user.application.UserService;
+import com.plus.profile.user.presentation.dto.PointChargeRequest;
 import com.plus.profile.user.presentation.dto.UserDetailResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -17,5 +17,9 @@ public class UserController {
     @GetMapping("/api/v1/users/{userId}")
     public ApiResponse<UserDetailResponse> getUserDetail(@PathVariable UUID userId) {
         return ApiResponse.success(userService.getUserDetail(userId));
+    }
+    @PostMapping("/api/v1/users/{userId}/points")
+    public ApiResponse<CreatePaymentResponse> chargePoint(@PathVariable UUID userId, @RequestBody PointChargeRequest request) {
+        return ApiResponse.success(userService.chargePoint(userId));
     }
 }
